56fb7632f86f5055340408e99db74981426ccfd3
georgis <georgis@microsoft.com>
2012-12-13 13:07:22 -0800

    Removing old code

--- applications/Tx/Source/Setup/NuGet/Tx-All-BuildLayout.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-
-set TxRelease=%3
-
-for %%a in (%1) do set BinariesLayoutFolder=%%~a
-for %%a in (%2) do set OutputSetupFolder=%%~a\lib
--- applications/Tx/Source/cleanTx.cmd
+++
@@ -1,4 +0,0 @@
-rd /s /q "%ProgramData%\LINQPad\Drivers\DataContext\4.0\TxLinqPadDriver (3d3a4b0768c9178e)"
-rd /s /q "%temp%\TxTypeCache"
-rd /s /q "%ProgramFiles(x86)%\Microsoft SDKs\LINQ to Traces"
-rd /s /q c:\TxSamples
